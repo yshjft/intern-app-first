549640a76e41dc39d8c61303ab82503a873872bd
hong yuseok <jericoh@naver.com>
2020-11-22 23:55:17 +0900

    세팅 실패

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import style from './test.scss'
 
 function App() {
   return (
     <div className="App">
-      <div className={`${style.test}`}>test</div>
+      <div>test</div>
     </div>
   )
 }
